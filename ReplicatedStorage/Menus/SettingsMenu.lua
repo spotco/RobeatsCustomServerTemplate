6da725817d1ff947c88b4727b02c3297f7c18d01
thenerdie <stirmanjr@gmail.coom>
2020-09-14 22:11:45 -0400

    Implement settings menu functionality

--- ReplicatedStorage/Menus/SettingsMenu.lua
+++ ReplicatedStorage/Menus/SettingsMenu.lua
@@ -6,15 +6,62 @@ local SPUtil = require(game.ReplicatedStorage.Shared.SPUtil)
 local RobeatsGame = require(game.ReplicatedStorage.RobeatsGameCore.RobeatsGame)
 local AudioManager = require(game.ReplicatedStorage.RobeatsGameCore.AudioManager)
 
-function SettingsMenu:new()
+local SongSelectMenu = require(game.ReplicatedStorage.Menus.SongSelectMenu)
+
+function SettingsMenu:new(_local_services)
 	local self = MenuBase:new()
 	
 	local back_hit = false
+
+	local settings = workspace.Settings
 
 	local _settings_ui
 	
 	function self:cons()
 		_settings_ui = EnvironmentSetup:get_menu_protos_folder().SettingsUI:Clone()
+		local keybinds = _settings_ui.Keybinds
+		local offset = _settings_ui.Offset
+		local notespeed = _settings_ui.Notespeed
+		local back = _settings_ui.Back
+
+		local function updateNSMULT()
+			notespeed.Display.Text = 1/settings.NoteSpeedMultiplier.Value
+		end
+
+		local function updateADOFFSET()
+			offset.Display.Text = settings.AudioOffset.Value
+		end
+
+		--TODO: CLEAN THIS UP
+
+		--//NOTESPEED
+		notespeed.Minus.MouseButton1Click:Connect(function()
+			settings.NoteSpeedMultiplier.Value = settings.NoteSpeedMultiplier.Value + 0.1
+			updateNSMULT()
+		end)
+
+		notespeed.Plus.MouseButton1Click:Connect(function()
+			settings.NoteSpeedMultiplier.Value = settings.NoteSpeedMultiplier.Value - 0.1
+			updateNSMULT()
+		end)
+
+		--//OFFSET
+		offset.Minus.MouseButton1Click:Connect(function()
+			settings.AudioOffset.Value = settings.AudioOffset.Value - 5
+			updateADOFFSET()
+		end)
+
+		offset.Plus.MouseButton1Click:Connect(function()
+			settings.AudioOffset.Value = settings.AudioOffset.Value + 5
+			updateADOFFSET()
+		end)
+
+		back.MouseButton1Click:Connect(function()
+			_local_services._menus:push_menu(SongSelectMenu:new(_local_services))
+		end)
+
+		updateNSMULT()
+		updateADOFFSET()
 	end
 	
 	--[[Override--]] function self:update(dt_scale)
