5fc561484a5eedc37f1a035ecf92d7401b3a9614
thenerdie <stirmanjr@gmail.coom>
2020-09-16 16:10:03 -0400

    Add configuration file

---
+++ ReplicatedStorage/Configuration.lua
@@ -0,0 +1,17 @@
+local config = {
+    AudioOffset = 0;
+    CreatorName = "<your name here>";
+    NoteGreatMaxMS = 140;
+    NoteGreatMinMS = -70;
+    NoteOkayMaxMS = 260;
+    NoteOkayMinMS = -140;
+    NotePerfectMaxMS = 40;
+    NotePerfectMinMS = -20;
+    NoteRemoveTimeMS = -200;
+    NoteSpeedMultiplier = 1;
+    PostFinishWaitTimeMS = 300;
+    PreStartCountdownTimeMS = 3000;
+    SupporterGamepassID = 11742318;
+}
+
+return config
