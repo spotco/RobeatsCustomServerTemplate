cf0611a3cad82284d8ffefdf8401532341932754
thenerdie <stirmanjr@gmail.coom>
2020-10-14 19:08:03 -0400

    Fix the implementation for the Enum handler

--- ReplicatedStorage/Serialization/Datastore/Handlers/EnumItem.lua
+++ ReplicatedStorage/Serialization/Datastore/Handlers/EnumItem.lua
@@ -6,12 +6,17 @@ function EnumItemHandler:serialize(item)
 end
 
 function EnumItemHandler:deserialize(item)
+    -- IF IM WRONG ABOUT THIS, AND SOMETHING BREAKS BECAUSE I WAS STUPID AND DONT KNOW HOW ENUMS WORK PLEASE FEEL FREE TO CORRECT ME
     local type = item.enum_type
     local value = item.value
 
     for i, v in pairs(Enum:GetEnums()) do
-        if v.EnumType == type and v.Value == value then
-            return v
+        if tostring(v) == type then
+            for _, enumItem in pairs(v:GetEnumItems()) do
+                if enumItem.Value == value then
+                    return enumItem
+                end
+            end
         end
     end
 end
