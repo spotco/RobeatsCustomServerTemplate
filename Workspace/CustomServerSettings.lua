e7a28b99c45c3085d7554f4bec55a86658aa09ad
spotco <mootothemax@gmail.com>
2020-10-17 02:53:11 -0400

    ship

---
+++ Workspace/CustomServerSettings.lua
@@ -0,0 +1,11 @@
+return {
+		--Put in your name here.
+		CreatorName = "<Your Name Here>";
+		--Put in your supported gamepass id here - players will buy this gamepass for supporter.
+		SupporterGamepassID = 11742318;
+		
+		LeaderboardSettings = {
+			TrackedPlayCount = 100;
+			SortByAccuracy = false;
+		}
+}
